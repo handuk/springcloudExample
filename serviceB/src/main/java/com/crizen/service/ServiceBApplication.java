2ed32cda4677290dc0b0e53dacba524bbc4eb7c2
dhan <dhan@crizen.com>
2020-09-25 16:22:10 +0900

    hystrix 예제 적용

--- serviceB/src/main/java/com/crizen/service/ServiceBApplication.java
+++ serviceB/src/main/java/com/crizen/service/ServiceBApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
@@ -12,6 +13,7 @@ import org.springframework.web.client.RestTemplate;
 @SpringBootApplication
 @EnableEurekaClient
 @EnableFeignClients(basePackages = {"com.crizen.service.feign"})
+@EnableHystrix
 public class ServiceBApplication {
 
 	public static void main(String[] args) {
--- serviceB/src/main/java/com/crizen/service/controller/ApiController.java
+++ serviceB/src/main/java/com/crizen/service/controller/ApiController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
 import com.crizen.service.feign.FeignRemoteServiceA;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 
 @RestController
 @RequestMapping("/api")
@@ -15,11 +16,28 @@ public class ApiController {
 	@Autowired
 	FeignRemoteServiceA feignServiceA;
 	
+	
+	/*
+	 * 
+	 * hystrix 옵션값 설정 example
+	   @HystrixCommand(fallbackMethod = "fallback", commandProperties = {
+		   @HystrixProperty(name = "execution.isolation.thread.timeoutInMilliseconds", value = "500"),
+		   @HystrixProperty(name = "metrics.rollingStats.timeInMilliseconds", value = "10000"),
+		   @HystrixProperty(name = "circuitBreaker.errorThresholdPercentage", value = "10"),
+		   @HystrixProperty(name = "circuitBreaker.requestVolumeThreshold", value = "5"),
+		   @HystrixProperty(name = "circuitBreaker.sleepWindowInMilliseconds", value = "10000")
+		}, threadPoolProperties = @HystrixProperty(name = "coreSize", value = "100"))
+	 */
 
 	@RequestMapping(path="/test", method=RequestMethod.GET)
+	@HystrixCommand(fallbackMethod = "testMethodFallback")//hystrix timeout은 기본1초이기때문에 연산이조금 걸리는 method 는 timeout 옵션을좀 늘려줘야함.
 	public String testMethod() {
         return "Service-B: 호출" + " > " + feignServiceA.testMethod();
-
+	}
+	
+	
+	public String testMethodFallback() {//서킷브레이커가 열렸을떄 기본초는 5초이다.
+		return "Service-B: 호출 에러";
 	}
 	
 }
